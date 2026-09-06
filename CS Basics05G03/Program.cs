698703ef69f98ee1627377dd1f4bf4ab999d7437
passant ahmed <passantahmed992006@outlook.com>
2026-09-06 20:02:44 +0300

    Add project files.

---
+++ CS Basics05G03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basics05G03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
